6a5ea425028f6c973e2ab39f2e0f2a35d7f2c9e5
vishnu <vishnuchilakala444@gmail.com>
2026-03-24 13:22:46 +0530

    added new internship experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -41,6 +41,35 @@ export default function Experience() {
                         <span className="exp-arrow">↗</span>
 
                     </a>
+
+                    {/* SmartBridge Internship 2 */}
+
+                    <a
+                        href="https://drive.google.com/file/d/1DzzhvL4wuVDG5SQYkzWEj2Ke9x8Ly6vZ/view?usp=sharing"
+                        className="exp-card reveal"
+                        style={{ transitionDelay: ".05s" }}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+
+                        <div className="exp-role">
+                            Full Stack Developement (MERN) Intern
+                        </div>
+
+                        <div className="exp-company">
+                            SmartBridge Educational Services Pvt. Ltd. · 2026
+                        </div>
+
+                        <div className="exp-desc">
+                            Completed a 6-month long-term internship on MERN stack
+                            development, organized in collaboration with APSCHE. Gained hands-on
+                            experience in building full stack applications using MongoDB, Express,
+                            React, and Node.js.
+                        </div>
+
+                        <span className="exp-arrow">↗</span>
+
+                    </a>
                 </div>
 
             </div>
